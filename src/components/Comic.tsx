f7f7579009af9cf3751f9fb4d0a36d672a3e8fb4
Arti Sahu <upasnagupta3333@gmail.com>
2023-11-18 17:26:46 +0530

    next changes

--- src/components/Comic.tsx
+++ src/components/Comic.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Comic.css'
-import {Link, useLocation} from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import Loading from './Loading'
 import { Slide } from 'react-slideshow-image';
@@ -8,6 +8,14 @@ import 'react-slideshow-image/dist/styles.css';
 import { GrLinkPrevious } from "react-icons/gr";
 import { GrLinkNext } from "react-icons/gr";
 import { CiStickyNote } from "react-icons/ci";
+import {
+    FaWhatsapp,
+    FaPinterest,
+    FaTelegram
+} from 'react-icons/fa'
+import { MdOutlineAttachEmail } from "react-icons/md";
+import { EmailShareButton, PinterestShareButton, TelegramShareButton, WhatsappShareButton } from 'react-share'
+
 
 
 const responsiveSettings = [
@@ -22,43 +30,59 @@ const responsiveSettings = [
 
 const indicators = () => (<div className="indicator"></div>);
 
-const Comic : React.FC = () => {
+const image = [
+    {
+        url: 'https://images.unsplash.com/photo-1682685795557-976f03aca7b2?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        text: 'This is a test'
+    },
+    {
+        url: 'https://images.unsplash.com/photo-1682685795557-976f03aca7b2?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        text: 'This is a test'
+    },
+]
+
+type imageInfo = {
+    url: string,
+    text: string
+}
+
+const Comic: React.FC = () => {
     // get state from the router
     const location = useLocation()
     const state = location.state
 
-    // State to store the comic image url
-    const [urls, setUrls] = useState<string[]>([])
+    // State to store the comic image url and text
+    const [images, setImages] = useState<imageInfo[]>([])
     const [fetching, setFetching] = useState<boolean>(false)
     const [error, setError] = useState<string>("")
 
     // useEfect to fetch the comic images
     useEffect(() => {
         // if there are no questions, return
-        if(!state || !state.questions || state.questions.length == 0){
+        if (!state || !state.questions || state.questions.length == 0) {
             setFetching(() => false)
-            setError("No prompts provided!")    
+            setError("No prompts provided!")
             return
         }
 
         // fetch the images
         const fetchImages = async () => {
-            try{
-                
+            try {
+
                 // set fetching to true
                 setFetching(() => true)
                 // set error to null
                 setError(() => '')
                 // create an array of promises
-                const promises = state?.questions.map(async (question: {inputs: string}) => {
+                const promises = state?.questions.map(async (question: { inputs: string }) => {
                     // fetch the image
                     const response = await fetch(
                         "https://xdwvg9no7pefghrn.us-east-1.aws.endpoints.huggingface.cloud",
                         {
-                            headers: { 
+                            headers: {
                                 "Accept": "image/png",
-                                "Authorization": "Bearer VknySbLLTUjbxXAXCjyfaFIPwUTCeRXbFSOjwRiCxsxFyhbnGjSFalPKrpvvDAaPVzWEevPljilLVDBiTzfIbWFdxOkYJxnOPoHhkkVGzAknaOulWggusSFewzpqsNWM", 
-                                "Content-Type": "application/json" 
+                                "Authorization": "Bearer VknySbLLTUjbxXAXCjyfaFIPwUTCeRXbFSOjwRiCxsxFyhbnGjSFalPKrpvvDAaPVzWEevPljilLVDBiTzfIbWFdxOkYJxnOPoHhkkVGzAknaOulWggusSFewzpqsNWM",
+                                "Content-Type": "application/json"
                             },
                             method: "POST",
                             body: JSON.stringify(question),
@@ -66,32 +90,37 @@ const Comic : React.FC = () => {
                     );
                     const result = await response.blob();
                     const url = URL.createObjectURL(result);
-                    return url
+                    return {
+                        url,
+                        text: question.inputs
+                    }
                 })
                 // wait for all the promises to resolve
-                const urls = await Promise.all(promises)
+                const images = await Promise.all(promises)
                 // set fetching to false
                 setFetching(() => false)
-                // set the urls
-                setUrls(urls)
-            }catch(e){
+                // set the images
+                setImages(images)
+            } catch (e) {
                 setFetching(() => false)
                 setError("Something went wrong")
             }
         }
         // call the function
-        fetchImages()
+        // fetchImages()
+        setImages(() => image)
         // unsubscribe: reset the state
         return () => {
-            setUrls(() => [])
+            setImages(() => [])
             setFetching(() => false)
             setError(() => '')
         }
     }, [state])
+
     return (
         <div className='Comic'>
-            <h1 className='title'> <CiStickyNote/> Your Comic </h1>
-            {fetching && <Loading/>}
+            <h1 className='title'> <CiStickyNote /> Your Comic </h1>
+            {fetching && <Loading />}
             {
                 !fetching && error == '' &&
                 <Slide
@@ -105,15 +134,17 @@ const Comic : React.FC = () => {
                     slidesToScroll={1}
                     responsive={responsiveSettings}
                     pauseOnHover={true}
-                    prevArrow={<GrLinkPrevious className='prev-arrow'/>}
-                    nextArrow={<GrLinkNext className='next-arrow'/>}
+                    prevArrow={<GrLinkPrevious className='prev-arrow' />}
+                    nextArrow={<GrLinkNext className='next-arrow' />}
 
                 >
                     {
-                        urls.map((url: string, index: number) => (
+                        images.map((image: imageInfo, index: number) => (
                             <div className="each-slide" key={index}>
-                                <div style={{'backgroundImage': `url(${url})`}}>
-                                    {/* <span>Slide {index + 1}</span> */}
+                                <div className='image' style={{ 'backgroundImage': `url(${image.url})` }}>
+                                    <div className='overlay'>
+                                        <p>{image.text}</p>
+                                    </div>
                                 </div>
                             </div>
                         ))
@@ -124,6 +155,38 @@ const Comic : React.FC = () => {
                 error && <div className='error'>
                     <h1>{error}</h1>
                     <Link className='button' to='/'>Go back to home</Link>
+                </div>
+            }
+            {
+                images && images.length > 0 &&
+                <div className='share'>
+                    {/* Join links of all images */}
+                    <WhatsappShareButton url={images.map((image: imageInfo, idx) => {
+                        return `${idx + 1}. ${image.url}`
+                    }).join('\n')} title='Check out my comic!'>
+                        <FaWhatsapp className='button' />
+                    </WhatsappShareButton>
+
+                    {/* Share on instagram */}
+                    <TelegramShareButton url={images.map((image: imageInfo, idx) => {
+                        return `${idx + 1}. ${image.url}`
+                    }).join('\n')} title='Check out my comic!'>
+                        <FaTelegram className='button' />
+                    </TelegramShareButton>
+                    <PinterestShareButton media={images.map((image: imageInfo, idx) => {
+                        return `${idx + 1}. ${image.url}`
+                    }).join('\n')} title='Check out my comic!' url={images.map((image: imageInfo, idx) => {
+                        return `${idx + 1}. ${image.url}`
+                    }
+                    ).join('\n')}>
+                        <FaPinterest className='button' />
+                    </PinterestShareButton>
+                    <EmailShareButton subject='Check out my comic!'  url={images.map((image: imageInfo, idx) => {
+                        return `${idx + 1}. ${image.url}`
+                    }
+                    ).join('\n')}>
+                        <MdOutlineAttachEmail className='button' />
+                    </EmailShareButton>
                 </div>
             }
         </div>
